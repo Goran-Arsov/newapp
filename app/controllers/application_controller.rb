87615a0d19176e56cf5c58d67485569bcee4f093
Goran-Arsov <arsphy@yahoo.com>
2022-11-22 23:51:09 +0100

    Add all sign_up params to devise sanitizer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,6 @@ class ApplicationController < ActionController::Base
 
     def configure_permitted_parameters
         devise_parameter_sanitizer.permit(:sign_in, keys: [:login])
-        devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :country])
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :country, :email, :password, :password_confirmation])
     end
 end
