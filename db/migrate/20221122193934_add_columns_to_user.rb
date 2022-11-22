55fbb6ad395318a27346aa17e497a4e21ebdda9e
Goran-Arsov <arsphy@yahoo.com>
2022-11-22 20:57:44 +0100

    Add username and country fields to users table. Set up devise parameter sanitizer

---
+++ db/migrate/20221122193934_add_columns_to_user.rb
@@ -0,0 +1,6 @@
+class AddColumnsToUser < ActiveRecord::Migration[7.0]
+  def change
+    add_column :users, :username, :string, null: false
+    add_column :users, :country, :string, null: false
+  end
+end
