9901e05fc4af4d21bf87a2b9d2e5b220a0ec5bec
Goran-Arsov <arsphy@yahoo.com>
2022-11-22 22:16:45 +0100

    Configure devise and views to log in through email or username

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,8 @@ class ApplicationController < ActionController::Base
     protected
 
     def configure_permitted_parameters
-
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :country])
+        # devise_parameter_sanitizer.permit(:sign_in, keys: [:username, :email, :password, :repeat_password])
+        devise_parameter_sanitizer.permit(:sign_in, keys: [:login])
+        devise_parameter_sanitizer.permit(:sign_in, keys: [:username, :password])
     end
 end
